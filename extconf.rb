8c6e9623802708f04113081053138c4ddb2f92fb
Yoshinori Kawasaki <ykawasaki@gmail.com>
2013-07-10 02:04:08 +0900

    Attempt to build for mecab installed with homebrew on OSX

--- extconf.rb
+++ extconf.rb
@@ -3,6 +3,8 @@ require 'mkmf'
 mecab_config = with_config('mecab-config', 'mecab-config')
 use_mecab_config = enable_config('mecab-config')
 
+$LDFLAGS += ' -L' + `#{mecab_config} --libs-only-L`.chomp
+
 `mecab-config --libs-only-l`.chomp.split.each { | lib |
   have_library(lib)
 }
